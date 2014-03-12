254478cc78fff53e307ca3eb07b78ead794e9446
GromHoll <gromholl@gmail.com>
2014-03-13 03:27:18 +0400

    Add random card drawing:

--- CardMatcher/CardMatcherViewController.h
+++ CardMatcher/CardMatcherViewController.h
@@ -1,11 +1,3 @@
-//
-//  CardMatcherViewController.h
-//  CardMatcher
-//
-//  Created by Admin on 13.03.14.
-//  Copyright (c) 2014 NTSU. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface CardMatcherViewController : UIViewController
--- CardMatcher/CardMatcherViewController.m
+++ CardMatcher/CardMatcherViewController.m
@@ -1,30 +1,43 @@
-//
-//  CardMatcherViewController.m
-//  CardMatcher
-//
-//  Created by Admin on 13.03.14.
-//  Copyright (c) 2014 NTSU. All rights reserved.
-//
-
 #import "CardMatcherViewController.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @interface CardMatcherViewController ()
+
+    @property (weak, nonatomic) IBOutlet UILabel * flipLabel;
+    @property (nonatomic) int flipCounter;
+    @property (strong, nonatomic) PlayingCardDeck * deck;
 
 @end
 
 @implementation CardMatcherViewController
 
-- (IBAction)touchCardButton:(UIButton *)sender {
-    if ([sender.currentTitle length]) {
-        UIImage * cardImage = [UIImage imageNamed:@"CardBack"];
-        [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
-        [sender setTitle: @"" forState:UIControlStateNormal];
-    } else {
-        UIImage * cardImage = [UIImage imageNamed:@"CardBlank"];
-        [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
-        [sender setTitle: @"qwe" forState:UIControlStateNormal];
+    - (PlayingCardDeck *)deck {
+        if (!_deck) {
+            _deck = [[PlayingCardDeck alloc] init];
+        }
+        return _deck;
     }
-}
+
+    - (void)setFlipCounter:(int)flipCounter {
+        _flipCounter = flipCounter;
+        self.flipLabel.text = [NSString stringWithFormat:@"Flip counter: %d", self.flipCounter];
+    
+    }
+
+    - (IBAction)touchCardButton:(UIButton *)sender {
+        if ([sender.currentTitle length]) {
+            UIImage * cardImage = [UIImage imageNamed:@"CardBack"];
+            [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
+            [sender setTitle: @"" forState:UIControlStateNormal];
+        } else {
+            UIImage * cardImage = [UIImage imageNamed:@"CardBlank"];
+            [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
+            Card * card = [self.deck drawRandomCard];
+            [sender setTitle: card.contents forState:UIControlStateNormal];
+        }
+        self.flipCounter++;
+    }
 
 
 @end
